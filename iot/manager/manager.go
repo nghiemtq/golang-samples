4a25ad9600ceacf322453dae7891b71ceb9bdd13
nghiemtq <36317195+nghiemtq@users.noreply.github.com>
2018-03-13 11:11:23 +0700

    Update manager.go

--- iot/manager/manager.go
+++ iot/manager/manager.go
@@ -1,6 +1,7 @@
 // Copyright 2018 Google Inc. All rights reserved.
 // Use of this source code is governed by the Apache 2.0
 // license that can be found in the LICENSE file.
+// Nghiem dep trai qua 
 
 // Command manager lets you manage Cloud IoT Core devices and registries.
 package main
